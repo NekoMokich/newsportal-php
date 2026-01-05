d2daeef22f906432218872b48ba1b6b4258c8348
Oleg Konnov <oleg.konnov@ivkhk.ee>
2026-01-05 18:33:10 +0200

    db

--- view/layout.php
+++ view/layout.php
@@ -2,4 +2,43 @@
 <html>
     <head>
         <title> NEWSPORTAL</title>
-        <link rel="stylesheet" href=">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
+        crossorigin="anonymous">
+        <link rel="stylesheet" type="text/css" href="/css/style.css">
+        <link href="https://fornts.googleapis.com/css?family=Noto+Serif" rel="stylesheet">
+        <meta charset="utf-8">
+        
+</head>
+    <body>
+        <nav class="one">
+            <ul class="top">
+        <li><a href="#">Kategooriad<i class="fa fa-angle-down"></i> </a>
+        <ul class="submenu">
+            <?php
+            Controller::AllCategory();
+            ?>
+        </ul>
+        </li>
+        <li><a href="testError">Info</a></li>
+        <li><a href="./">Stardileht</a></li>
+
+        </ul>
+        </nav>
+        <section>
+            <div class= 'divBox'>
+            <?php
+            if (isset($content)) {
+                echo $content;
+            }
+            else {
+                echo '<hl>Content is gone!</hl>';
+            }
+            ?>
+            </div>
+        </section>
+            <hr>
+            <p style="display:block; text-align:center;">JKITp24 2026 a. &copy</p>
+    </body>
+</html> 
+
